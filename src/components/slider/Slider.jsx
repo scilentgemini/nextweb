91a5fb7f5de6dd9108d003ece3d951224d122bd8
scilentgemini <rbchitthi@gmail.com>
2025-02-24 01:36:02 +0545

    added actuall images in slider and fixed some scss

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -4,6 +4,9 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import * as Icon from "@phosphor-icons/react/dist/ssr";
+import Image from "next/image";
+import Link from "next/link";
+import 'swiper/css/bundle';
 
 const Slider = () => {
   return (
@@ -30,10 +33,110 @@ const Slider = () => {
               delay: 4000,
             }}
           >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>
+              <div className="slider-item slider-first">
+                <div className="bg-img">
+                  <Image
+                    src={"/images/slider/one.jpg"}
+                    width={4000}
+                    height={3000}
+                    alt="slider1"
+                    priority={true}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+
+                <div className="container">
+                  <div className="text-content flex-column-between">
+                    <div className="heading2">
+                      <div className="relative overflow-hidden">
+                        <span className="block relative overflow-hidden">
+                          Simplify and Secure
+                        </span>
+                        <span className="block absolute top-0 left-0 w-full h-full">
+                          Simplify and Secure
+                        </span>
+                      </div>
+
+                      <div className="relative overflow-hidden">
+                        <span className="block relative overflow-hidden">
+                          Our Solution
+                        </span>
+                        <span className="block absolute top-0 left-0 w-full h-full">
+                          Our Solution
+                        </span>
+                      </div>
+                    </div>
+
+                    <div className="body2 mt-3 text-secondary">
+                        lorem   loren tesc hi bracker this is a breacker <br /> same lok i don;t know what im ven typing cause my eyes are closed while i typ tjis
+                    </div>
+
+                    <div className="button-block md:mt-10 mt-6">
+                        <Link className="button-main bg-blue-700 text-white hover:bg-blue-500"
+                        href="/service">
+                            Discover Now
+                        </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <div className="slider-item slider-first">
+                <div className="bg-img">
+                  <Image
+                    src={"/images/slider/two.jpg"}
+                    width={4000}
+                    height={3000}
+                    alt="slider1"
+                    priority={true}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+
+                <div className="container">
+                  <div className="text-content flex-column-between">
+                    <div className="heading2">
+                      <div className="relative overflow-hidden">
+                        <span className="block relative overflow-hidden">
+                          Easy to Use
+                        </span>
+                        <span className="block absolute top-0 left-0 w-full h-full">
+                        Easy to Use
+                        </span>
+                      </div>
+
+                      <div className="relative overflow-hidden">
+                        <span className="block relative overflow-hidden">
+                          Our Solution
+                        </span>
+                        <span className="block absolute top-0 left-0 w-full h-full">
+                          Our Solution
+                        </span>
+                      </div>
+                    </div>
+
+                    <div className="body2 mt-3 text-secondary">
+                        lorem   loren tesc hi bracker this is a breacker <br /> same lok i don;t know what im ven typing cause my eyes are closed while i typ tjis
+                    </div>
+
+                    <div className="button-block md:mt-10 mt-6">
+                        <Link className="button-main bg-blue-700 text-white hover:bg-blue-500"
+                        href="/service">
+                            Discover Now
+                        </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
           </Swiper>
+        </div>
+
+        <div className="next-arrow items-center justify-center">
+          <Icon.CaretRight className="text-white heading6" weight="bold" />
         </div>
       </div>
     </>
